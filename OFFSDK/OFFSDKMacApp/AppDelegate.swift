14533c469095e7afd0d3d67495b62c87d11d80be
Mihaela MJ <mihaelamj@me.com>
2020-11-27 18:15:55 +0100

    added mac app target

---
+++ OFFSDK/OFFSDKMacApp/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  OFFSDKMacApp
+//
+//  Created by Mihaela Mihaljevic Jakic on 27.11.2020..
+//  Copyright © 2020 Mihaela Mihaljevic Jakic. All rights reserved.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ OFFSDK/OFFSDKMacApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  OFFSDKMacApp
+//
+//  Created by Mihaela Mihaljevic Jakic on 27.11.2020..
+//  Copyright © 2020 Mihaela Mihaljevic Jakic. All rights reserved.
+//
+
+import Cocoa
+import OFFSDKmacOS
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
